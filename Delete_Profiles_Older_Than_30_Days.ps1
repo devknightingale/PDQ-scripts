a45b7d4162bfe57380c9a44c209ab07744fbb199
kroland <kate.roland1@outlook.com>
2025-06-02 10:54:44 -0400

    changed error behavior

--- Delete_Profiles_Older_Than_30_Days.ps1
+++ Delete_Profiles_Older_Than_30_Days.ps1
@@ -1,3 +1,6 @@
+#Requires -RunAsAdministrator
+#testing below for continuing the script after it runs into errors
+$ErrorActionPreference = 'SilentlyContinue'
 
 
 # Define the number of days of inactivity
@@ -7,8 +10,7 @@ $daysInactive = 14
 $deleteCount = 0
 
 # Retrieve all user profiles from the registry
-# remember to add back the Local Admin account name (removed from this file for privacy purposes)
-$userProfiles = Get-CimInstance Win32_UserProfile | Where-Object { $_.Special -eq $false -and $_.LocalPath -notlike "*System32*" -and $_.LocalPath -notlike "*Administrator*" -and $_.LocalPath -notlike "*Public*"}
+$userProfiles = Get-CimInstance Win32_UserProfile | Where-Object { $_.Special -eq $false -and $_.LocalPath -notlike "*System32*" -and $_.LocalPath -notlike "*TFHC*" -and $_.LocalPath -notlike "*Administrator*" -and $_.LocalPath -notlike "*Public*"}
 
 foreach ($profile in $userProfiles) {
     $file = Get-Item ($profile.LocalPath)
@@ -17,10 +19,12 @@ foreach ($profile in $userProfiles) {
         try {
             # Delete the profile
             $profile | Remove-CimInstance
-            Write-Host "Profile deleted successfully: $($profile.LocalPath)" -ForegroundColor Green
             $deleteCount += 1
+            Write-Host "Profile deleted successfully: $($profile.LocalPath)" -ForegroundColor Green
+            
         } catch {
             Write-Host "Failed to delete profile: $($profile.LocalPath). Error: $_" -ForegroundColor Red
+            continue
         }
    }
 }
